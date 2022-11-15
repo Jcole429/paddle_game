dd05c5bb72c49274065c4b9be6449e2473cfbe7b
Justin Cole <jcole429@student.fdu.edu>
2022-11-15 17:02:56 -0500

    Creating initial ball logic

---
+++ ball.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+from constant import *
+
+
+class Ball(Turtle):
+    def __init__(self, xcor=0, ycor=0):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.shapesize(stretch_wid=2, stretch_len=2)
+        self.penup()
+        self.goto(xcor, ycor)
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        if self.ycor() >= (SCREEN_HEIGHT / 2) - BALL_DIAMETER or self.ycor() <= (-SCREEN_HEIGHT / 2) + BALL_DIAMETER:
+            self.bounce()
+
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+
+        self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 from constant import *
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -11,6 +13,8 @@ screen.title("Pong")
 r_paddle = Paddle(350)
 l_paddle = Paddle(-350)
 
+ball = Ball()
+
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -20,6 +24,8 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
