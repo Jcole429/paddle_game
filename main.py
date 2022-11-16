be7d073d0f847ab645d62e493b14985e9969ad0a
Justin Cole <jcole429@student.fdu.edu>
2022-11-15 19:02:44 -0500

    Setting screen refresh rate with constant

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(SCREEN_REFRESH_RATE)
     screen.update()
 
     # Detect collision with r_paddle
